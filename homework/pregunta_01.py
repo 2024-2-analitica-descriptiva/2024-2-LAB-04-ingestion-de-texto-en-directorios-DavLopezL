abc92a04d94d8651b347754c3ce9bb818867b8bb
DavLopezL <davlopezlo@unal.edu.co>
2024-12-28 02:37:15 -0500

    Primer Intento

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,10 +4,124 @@
 """
 Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
-
+import pandas as pd
+import os
 
 def pregunta_01():
-    """
+
+    if not os.path.exists("files/output"):
+        os.makedirs("files/output")
+
+    data_test_neg = []
+
+    #Crear lista con nombres de archivos
+    list_test_negs = [ "{:04d}".format(number) for number in range(67)]
+    url_test_neg = "files/input/test/negative/{}.txt"
+
+    #Crear columna con información de cada archivo
+    for num in list_test_negs:
+        with open(url_test_neg.format(num),"r",encoding="utf-8") as archivo:
+            linea = archivo.readline()
+            data_test_neg.append(linea)
+    
+    #Convertirlo a Dataframe y crear columna de target
+    test_neg = pd.DataFrame(data_test_neg,columns=["phrase"])
+    test_neg["target"] = "negative"
+    
+    data_test_neu = []
+
+    #Crear lista con nombres de archivos
+    list_test_neu = [ "{:04d}".format(number) for number in range(274)]
+    url_test_neu = "files/input/test/neutral/{}.txt"
+
+    #Crear columna con información de cada archivo
+    for num in list_test_neu:
+        with open(url_test_neu.format(num),"r",encoding="utf-8") as archivo:
+            linea = archivo.readline()
+            data_test_neu.append(linea)
+    
+    #Convertirlo a Dataframe y crear columna de target
+    test_neu = pd.DataFrame(data_test_neu,columns=["phrase"])
+    test_neu["target"] = "neutral"
+
+
+    data_test_pos = []
+
+    #Crear lista con nombres de archivos
+    list_test_pos = [ "{:04d}".format(number) for number in range(112)]
+    url_test_pos = "files/input/test/positive/{}.txt"
+
+    #Crear columna con información de cada archivo
+    for num in list_test_pos:
+        with open(url_test_pos.format(num),"r",encoding="utf-8") as archivo:
+            linea = archivo.readline()
+            data_test_pos.append(linea)
+    
+    #Convertirlo a Dataframe y crear columna de target
+    test_pos = pd.DataFrame(data_test_pos,columns=["phrase"])
+    test_pos["target"] = "positive"
+
+    df_test = pd.concat(objs=[test_neg, test_neu, test_pos], axis=0)
+
+
+    data_train_neg = []
+
+    #Crear lista con nombres de archivos
+    list_train_negs = [ "{:04d}".format(number) for number in range(236)]
+    url_train_neg = "files/input/train/negative/{}.txt"
+
+    #Crear columna con información de cada archivo
+    for num in list_train_negs:
+        with open(url_train_neg.format(num),"r",encoding="utf-8") as archivo:
+            linea = archivo.readline()
+            data_train_neg.append(linea)
+    
+    #Convertirlo a Dataframe y crear columna de target
+    train_neg = pd.DataFrame(data_train_neg,columns=["phrase"])
+    train_neg["target"] = "negative"
+    
+    data_train_neu = []
+
+    #Crear lista con nombres de archivos
+    list_train_neu = [ "{:04d}".format(number) for number in range(1117)]
+    url_train_neu = "files/input/train/neutral/{}.txt"
+
+    #Crear columna con información de cada archivo
+    for num in list_train_neu:
+        with open(url_train_neu.format(num),"r",encoding="utf-8") as archivo:
+            linea = archivo.readline()
+            data_train_neu.append(linea)
+    
+    #Convertirlo a Dataframe y crear columna de target
+    train_neu = pd.DataFrame(data_train_neu,columns=["phrase"])
+    train_neu["target"] = "neutral"
+
+
+    data_train_pos = []
+
+    #Crear lista con nombres de archivos
+    list_train_pos = [ "{:04d}".format(number) for number in range(458)]
+    url_train_pos = "files/input/train/positive/{}.txt"
+
+    #Crear columna con información de cada archivo
+    for num in list_train_pos:
+        with open(url_train_pos.format(num),"r",encoding="utf-8") as archivo:
+            linea = archivo.readline()
+            data_train_pos.append(linea)
+    
+    #Convertirlo a Dataframe y crear columna de target
+    train_pos = pd.DataFrame(data_train_pos,columns=["phrase"])
+    train_pos["target"] = "positive"
+
+    df_train = pd.concat(objs=[train_neg, train_neu, train_pos], axis=0)
+
+    #Guardar los DataFrames en la carpeta files/output
+    df_test.to_csv("files/output/test_dataset.csv", encoding="utf-8")
+    df_train.to_csv("files/output/train_dataset.csv", encoding="utf-8")
+
+    return df_train, df_test
+
+"""
     La información requerida para este laboratio esta almacenada en el
     archivo "files/input.zip" ubicado en la carpeta raíz.
     Descomprima este archivo.
@@ -70,4 +184,4 @@ def pregunta_01():
     ```
 
 
-    """
+"""
